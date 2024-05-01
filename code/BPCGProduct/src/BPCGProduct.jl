1dd5b61f33e5c351a8b6a88730e89290e7db8bbd
giommazz <gab.iom89@gmail.com>
2024-05-01 12:39:07 +0200

    clean up code and repo

--- code/BPCGProduct/src/BPCGProduct.jl
+++ code/BPCGProduct/src/BPCGProduct.jl
@@ -1,18 +1,23 @@
-module BPCGProduct
+module test
 using FrankWolfe
 
 k = 2
-n = 2
-target_tolerance = 1e-6
-max_iterations = 10000
+n = 5000
+target_tolerance = 1e-9
+max_iterations = 500000
+# REMINDER FOR EXPERIMENTS: 
+# 1) INCREASE precision
+# 2) INCREASE N. OF MAX ITERATIONS SO THAT EVEN SUBLINEAR ALGO CONVERGES.
+# 3) WITH THESE TWO, YOU CAN SHOW THAT TO REACH COVERGENCE BPCG IS FASTER 
+
 
 # Function to compute the pairwise distance objective
 function f(x)
     sum_dist = 0.0
     for i in 1:k    
         for j in i+1:k
-            xi = x[i]
-            xj = x[j]
+            xi = x.blocks[i]
+            xj = x.blocks[j]
             curr = sum((xi - xj).^2)
             sum_dist += curr
         end
@@ -22,59 +27,49 @@ end
 
 # Gradient computation for tuple of vectors
 function grad!(storage, x)
-    # Initialize or reset 'storage' to zero vectors
-    storage = [zeros(Float64, n) for _ in 1:k]
-    println("°°°°°°°° storage: ", storage, typeof(storage))
-    
-    println("°°°°°°°°x: ", x, typeof(x))
     for i = 1:k
-        println("°°°°°°°° xᵢ: ", x[i], typeof(x))
         sum_terms = zeros(n)
         for j = 1:k
             if i != j
-                println("°°°°°°°° xⱼ", x[j], typeof(x))
-                sum_terms .+= x[j]
+                sum_terms .+= x.blocks[j]
             end
         end
-        storage[i] .= 0.5 * ((k-1) * x[i] - sum_terms)
+        storage.blocks[i] .= 0.5 * ((k-1) * x.blocks[i] - sum_terms)
     end
 end
 
-# Setup Linear Minimization Oracles for the polytopes
-function setup_lmos(n)
-    # Polytopes LMOs (https://github.com/ZIB-IOL/FrankWolfe.jl/blob/97a599c029a054aab6a5574d9bed8d48e0f9fb01/src/polytope_oracles.jl#L4)
-    lmo1 = FrankWolfe.ProbabilitySimplexOracle(1.0)
-    lmo2 = FrankWolfe.ProbabilitySimplexOracle(12.0)
-    #lmo3 = FrankWolfe.ProbabilitySimplexOracle(5.0)
-    #lmo4 = FrankWolfe.ScaledBoundLInfNormBall(-ones(n), zeros(n))
-    #lmo5 = FrankWolfe.ScaledBoundLInfNormBall(-ones(n), zeros(n))
-    #lmo6 = FrankWolfe.ScaledBoundLInfNormBall(-ones(n), 3*ones(n))
-    return (lmo1, lmo2#, lmo3, lmo4, lmo5, lmo6
-    )
+function create_product_lmo(lmos_list)
+    # Check if length of LMO list matches `k`
+    if length(lmos_list) != k
+        error("The number of LMOs provided ($(length(lmos_list))) does not match the expected number ($k).")
+    end
+    # Convert list of LMOs to a tuple, as required by `FrankWolfe.ProductLMO`
+    lmos_tuple = Tuple(lmos_list)
+    # Create and return a ProductLMO object
+    return FrankWolfe.ProductLMO(lmos_tuple)
 end
 
-# Initialize feasible points for each polytope
-function initialize_points(lmos, n)
-    return  tuple([FrankWolfe.compute_extreme_point(lmo, zeros(n)) for lmo in lmos]...)
+# Find starting point `x0` over the product of different LMOs
+function find_starting_point(prod_lmo)    
+    # Prepare datafor `x0`, which is of type `FrankWolfe.BlockVector{Float64, Vector{Float64}, Tuple{Int64}}`
+    # 1) Compute extreme points for each LMO in the product
+    extreme_points = [FrankWolfe.compute_extreme_point(lmo, zeros(n)) for lmo in prod_lmo.lmos]    
+    # 2) Generate a vector of length `k`, where each entry is a tuple `(n,)`
+    block_sizes = fill((n,), length(prod_lmo.lmos))
+    # 3) Compute total size of `x0`
+    total_size = sum([size[1] for size in block_sizes]) 
+
+    # Instantiate `x0`
+    return FrankWolfe.BlockVector(extreme_points, block_sizes, total_size)
 end
 
-# Run ALM
-function run_FW(method, order, lmos, x0, k, n, target_tolerance, max_iterations)   
-    
-    storage = [zeros(Float64, n) for _ in 1:k]
-    f = x -> objective_function(x, k, n)
-    grad! = (storage, x) -> gradient!(storage, x, k, n)
-    
+# Run BC (ALM setting) with vanilla FW
+function run_FW(order, prod_lmo, x0, k, n, target_tolerance, max_iterations)   
     trajectories = []
-    println("°°°°°°°° x₀: ", x0, typeof(x0))
-
-    # Check (https://zib-iol.github.io/FrankWolfe.jl/dev/examples/docs_10_alternating_methods/): performing a full (simulatenous) BPCG update at each iteration, 
-    # 	by running `alternating_linear_minimization` with `blended_pairwise_conditional_gradient` inside
-    xx, v, primal, dual_gap, _, trajectory_data = FrankWolfe.alternating_linear_minimization(
-        method,
+    x, v, primal, dual_gap, trajectory_data = FrankWolfe.block_coordinate_frank_wolfe(
         f,
         grad!,
-        lmos,
+        prod_lmo,
         x0,
         update_order=order,
         epsilon=target_tolerance,
@@ -88,17 +83,34 @@ function run_FW(method, order, lmos, x0, k, n, target_tolerance, max_iterations)
     push!(trajectories, trajectory_data)    
     return trajectories
 end
-# Run BPCG
-function run_FW(method, lmos, x0, k, n, target_tolerance, max_iterations)   
+# Run BC (ALM setting) with BPCG
+function run_FW(order, update_step, prod_lmo, x0, k, n, target_tolerance, max_iterations)
     trajectories = []
-    println("°°°°°°°° x₀: ", x0, typeof(x0))
-    # Check (https://zib-iol.github.io/FrankWolfe.jl/dev/examples/docs_10_alternating_methods/): performing a full (simulatenous) BPCG update at each iteration, 
-    # 	by running `alternating_linear_minimization` with `blended_pairwise_conditional_gradient` inside
-    xx, v, primal, dual_gap, _, trajectory_data = FrankWolfe.alternating_linear_minimization(
-        method,
+    x, v, primal, dual_gap, trajectory_data = FrankWolfe.block_coordinate_frank_wolfe(
         f,
         grad!,
-        lmos,
+        prod_lmo,
+        x0,
+        update_order=order,
+        epsilon=target_tolerance,
+        max_iteration=max_iterations,
+        line_search=FrankWolfe.Shortstep(one(Int)),
+        print_iter=max_iterations / 10,
+        memory_mode=FrankWolfe.InplaceEmphasis(),
+        update_step=update_step,
+        verbose=true,
+        trajectory=true,
+    );
+    push!(trajectories, trajectory_data)    
+    return trajectories
+end
+# Run BPCG over full product LMO
+function run_FW(prod_lmo, x0, k, n, target_tolerance, max_iterations)   
+    trajectories = []
+    x, v, primal, dual_gap, _, trajectory_data = FrankWolfe.blended_pairwise_conditional_gradient(
+        f,
+        grad!,
+        prod_lmo,
         x0,
         epsilon=target_tolerance,
         max_iteration=max_iterations,
@@ -112,23 +124,41 @@ function run_FW(method, lmos, x0, k, n, target_tolerance, max_iterations)
     return trajectories
 end
 
-
-# Main execution function
 function main()
 
-    lmos = setup_lmos(n)
-    x0 = initialize_points(lmos, n)
+    # Polytopes LMOs (https://github.com/ZIB-IOL/FrankWolfe.jl/blob/97a599c029a054aab6a5574d9bed8d48e0f9fb01/src/polytope_oracles.jl#L4)
+	# Setup Linear Minimization Oracles for the polytopes
+    lmo1 = FrankWolfe.ProbabilitySimplexOracle(1.0)
+    lmo2 = FrankWolfe.ProbabilitySimplexOracle(123.0)
+    # lmo3 = FrankWolfe.ProbabilitySimplexOracle(123.0)
+    # lmo4 = FrankWolfe.ProbabilitySimplexOracle(123.0)
+    # lmo5 = FrankWolfe.ProbabilitySimplexOracle(123.0)
+    # lmo6 = FrankWolfe.ScaledBoundLInfNormBall(-ones(n), ones(n))
+    # lmo7 = FrankWolfe.ScaledBoundLInfNormBall(-ones(n), ones(n))
+    # lmo8 = FrankWolfe.ScaledBoundLInfNormBall(-ones(n), ones(n))
+    # lmo9 = FrankWolfe.ScaledBoundLInfNormBall(-ones(n), ones(n))
+    # lmo10 = FrankWolfe.ScaledBoundLInfNormBall(-ones(n), ones(n))
 
-    # Run BPCG
-    println("Running BPCG over the full polytope product...")
-    bpcg_trajectories = run_FW(FrankWolfe.blended_pairwise_conditional_gradient, lmos, x0, k, n, target_tolerance, max_iterations)
+    prod_lmo = create_product_lmo([lmo1, lmo2])
+    x0 = find_starting_point(prod_lmo)
 
-    # Run ALM with block-coordinate Frank-Wolfe
-    println("Running ALM with block-coordinate Frank-Wolfe...")
-    alm_trajectories = run_FW(FrankWolfe.block_coordinate_frank_wolfe, FrankWolfe.CyclicUpdate(), lmos, x0, k, n, target_tolerance, max_iterations)
+    # Block-coordinate vanilla FW
+    println("\n\n\n---------------------------------------------------------")
+    println("Run Alternating Linear Minimization (block-coordinate style) with vanilla FW")
+    println("---------------------------------------------------------")
+    bc_fw_trajectories = run_FW(FrankWolfe.CyclicUpdate(), prod_lmo, x0, k, n, target_tolerance, max_iterations)
 
-    println("Optimization completed.")
-    #plot_trajectories([bpcg_trajectories, alm_trajectories], ["BPCG", "ALM"], xscalelog=true) # Uncomment and define plot_trajectories if visualization is needed
+    # Block-coordinate BPCG
+    println("\n\n\n---------------------------------------------------------")
+    println("Run Alternating Linear Minimization (block-coordinate style) with BPCG)")
+    println("---------------------------------------------------------")
+    bc_bpcg_trajectories = run_FW(FrankWolfe.CyclicUpdate(), FrankWolfe.BPCGStep(), prod_lmo, x0, k, n, target_tolerance, max_iterations)
+    
+    # BPCG over full product LMO
+    println("\n\n\n---------------------------------------------------------")
+    println("Run Blended Pairwise Conditional Gradient over the full product LMO")
+    println("---------------------------------------------------------")
+    bpcg_trajectories = run_FW(prod_lmo, x0, k, n, target_tolerance, max_iterations)
 end
 
 end # module BPCGProduct
