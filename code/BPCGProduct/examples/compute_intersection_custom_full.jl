e2cd2a232e127a637cf5a4c288bf0d5f0c9e99a0
giommazz <gab.iom89@gmail.com>
2024-06-28 19:19:36 +0200

    small edits

--- code/BPCGProduct/examples/compute_intersection_custom_full.jl
+++ code/BPCGProduct/examples/compute_intersection_custom_full.jl
@@ -24,9 +24,9 @@ function main(config::Config, vertices, shifted_vertices, primal, labels, basena
         _, _, _, _, td_cyc_bc_fw = run_FW(config, FrankWolfe.CyclicUpdate(), prod_lmo)
         push_to_trajectories!(ni_flag, td_cyc_bc_fw, trajectories_ni, trajectories_i, primal)
 
-        # println("\n\n\n ----------> Cyclic Block-coordinate BPFW")
-        # _, _, _, _, td_cyc_bc_bpcg = run_FW(config, FrankWolfe.CyclicUpdate(), FrankWolfe.BPCGStep(), prod_lmo)
-        # push_to_trajectories!(ni_flag, td_cyc_bc_bpcg, trajectories_ni, trajectories_i, primal)
+        println("\n\n\n ----------> Cyclic Block-coordinate BPFW")
+        _, _, _, _, td_cyc_bc_bpcg = run_FW(config, FrankWolfe.CyclicUpdate(), FrankWolfe.BPCGStep(), prod_lmo)
+        push_to_trajectories!(ni_flag, td_cyc_bc_bpcg, trajectories_ni, trajectories_i, primal)
 
         println("\n\n\n ----------> Full Block-coordinate BPFW")
         _, _, _, _, td_full_bc_cg = run_FW(config, FrankWolfe.FullUpdate(), FrankWolfe.BPCGStep(), prod_lmo)  
@@ -36,17 +36,17 @@ function main(config::Config, vertices, shifted_vertices, primal, labels, basena
         # _, _, _, _, td_bpcg = run_FW(config, prod_lmo)    
         # push_to_trajectories!(ni_flag, td_bpcg, trajectories_ni, trajectories_i, primal)
         
-        println("\n\n\n ----------> AP")
-        _, _, _, _, td_ap = run_FW(config, prod_lmo, true)    
-        push_to_trajectories!(ni_flag, td_ap, trajectories_ni, trajectories_i, primal)
+        # println("\n\n\n ----------> AP")
+        # _, _, _, _, td_ap = run_FW(config, prod_lmo, true)    
+        # push_to_trajectories!(ni_flag, td_ap, trajectories_ni, trajectories_i, primal)
 
         # Save trajectories
         # save_trajectories("examples/traj_$basename.jld2", trajectories_ni, trajectories_i)
 
     end
     
-    log_data(trajectories_i, labels, "times_i_"*basename)
-    log_data(trajectories_ni, labels, "times_ni_"*basename)
+    log_data(trajectories_i, labels, "examples/results/times_i_"*basename)
+    log_data(trajectories_ni, labels, "examples/results/times_ni_"*basename)
 
     return trajectories_ni, trajectories_i
 end
@@ -67,7 +67,7 @@ primal = primal - 1
 basename = generate_filename(config, vertices)
 
 # Labels for the plots
-labels = ["C-BC-FW", "F-BC-BPFW", "AP"]# ["C-BC-FW", "C-BC-BPFW", "F-BC-BPFW", "F-BPFW", "AP"]
+labels = ["C-BC-FW", "C-BC-BPFW", "F-BC-BPFW"]# ["C-BC-FW", "C-BC-BPFW", "F-BC-BPFW", "F-BPFW", "AP"]
 
 # execute main
 println("\n\n********************************************************")
@@ -77,5 +77,5 @@ trajectories_ni, trajectories_i = main(config,vertices, shifted_vertices, primal
 
 
 # Plot trajectories
-plot_trajectories(trajectories_ni, labels, yscalelog=true, xscalelog=true, filename="plot_ni_$basename.png")
-plot_trajectories(trajectories_i, labels, yscalelog=true, xscalelog=true, filename="plot_i_$basename.png")
+plot_trajectories(trajectories_ni, labels, yscalelog=true, xscalelog=true, filename="examples/results/plot_ni_$basename.png")
+plot_trajectories(trajectories_i, labels, yscalelog=true, xscalelog=true, filename="examples/results/plot_i_$basename.png")
